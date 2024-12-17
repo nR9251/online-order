69c7c6c368e373e6408ffb030c0445ba381b8c9c
nR9251 <akisuiayane@gmail.com>
2024-12-18 00:38:14 +0800

    first version

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using prjonlineorder.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prjonlineorder.Controllers
+{
+    public class CustomerController : Controller
+    {
+        dbORD2Context db = new dbORD2Context();
+        public IActionResult Menu()
+        {
+            var Meal = db.TableC.ToList();
+            return View(Meal);
+        }
+
+        public IActionResult Order()
+        {
+            return View();
+        }
+        public IActionResult customerA()
+        {
+            SqlConnection sqlConn = new SqlConnection("Data Source=LAPTOP-LD3NQLFM;Initial Catalog=dbORD2;Integrated Security=True");
+            sqlConn.Open();
+            String strSQL = "UPDATE TableB" + 1 + " SET tPrice=TableC.tPrice*TableB" + 1 + ".tNum FROM TableC WHERE TableC.tMeal=TableB" + 1 + ".tMeal";
+            SqlCommand sqlcommand = new SqlCommand(strSQL, sqlConn);
+            sqlcommand.ExecuteNonQuery();
+            db.SaveChanges();
+            sqlConn.Close();
+            var Meal = db.TableB1.ToList();
+            return View(Meal);
+        }
+
+
+        public IActionResult customerB()
+        {
+            SqlConnection sqlConn = new SqlConnection("Data Source=LAPTOP-LD3NQLFM;Initial Catalog=dbORD2;Integrated Security=True");
+            sqlConn.Open();
+            String strSQL = "UPDATE TableB" + 2 + " SET tPrice=TableC.tPrice*TableB" + 2 + ".tNum FROM TableC WHERE TableC.tMeal=TableB" + 2 + ".tMeal";
+            SqlCommand sqlcommand = new SqlCommand(strSQL, sqlConn);
+            sqlcommand.ExecuteNonQuery();
+            db.SaveChanges();
+            sqlConn.Close();
+            var Meal = db.TableB2.ToList();
+            return View(Meal);
+        }
+        public IActionResult customerC()
+        {
+            SqlConnection sqlConn = new SqlConnection("Data Source=LAPTOP-LD3NQLFM;Initial Catalog=dbORD2;Integrated Security=True");
+            sqlConn.Open();
+            String strSQL = "UPDATE TableB" + 3 + " SET tPrice=TableC.tPrice*TableB" + 3 + ".tNum FROM TableC WHERE TableC.tMeal=TableB" + 3 + ".tMeal";
+            SqlCommand sqlcommand = new SqlCommand(strSQL, sqlConn);
+            sqlcommand.ExecuteNonQuery();
+            db.SaveChanges();
+            sqlConn.Close();
+            var Meal = db.TableB3.ToList();
+            return View(Meal);
+        }
+
+        public IActionResult EditA(int id)
+        {
+            var i = db.TableB1.Where(m => m.TNumber == id).FirstOrDefault();
+            return View(i);
+        }
+        [HttpPost]
+        public IActionResult EditA(TableB1 i)
+        {
+            var modify = db.TableB1.Where(m => m.TNumber == i.TNumber).FirstOrDefault();
+            modify.TNumber = i.TNumber;
+            modify.TNum = i.TNum;
+            db.SaveChanges();
+            return RedirectToAction("customerA");
+        }
+
+        public IActionResult EditB(int id)
+        {
+            var i = db.TableB2.Where(m => m.TNumber == id).FirstOrDefault();
+            return View(i);
+        }
+        [HttpPost]
+        public IActionResult EditB(TableB2 i)
+        {
+            var modify = db.TableB2.Where(m => m.TNumber == i.TNumber).FirstOrDefault();
+            modify.TNumber = i.TNumber;
+            modify.TNum = i.TNum;
+            db.SaveChanges();
+            return RedirectToAction("customerB");
+        }
+
+        public IActionResult EditC(int id)
+        {
+            var i = db.TableB3.Where(m => m.TNumber == id).FirstOrDefault();
+            return View(i);
+        }
+        [HttpPost]
+        public IActionResult EditC(TableB3 i)
+        {
+            var modify = db.TableB3.Where(m => m.TNumber == i.TNumber).FirstOrDefault();
+            modify.TNumber = i.TNumber;
+            modify.TNum = i.TNum;
+            db.SaveChanges();
+            return RedirectToAction("customerC");
+        }
+    }
+}
---
+++ Controllers/StoreController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using prjonlineorder.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prjonlineorder.Controllers
+{
+    public class StoreController : Controller
+    {
+        dbORD2Context db = new dbORD2Context();
+        public IActionResult Backstage()
+        {
+            SqlConnection sqlConn = new SqlConnection("Data Source=LAPTOP-LD3NQLFM;Initial Catalog=dbORD2;Integrated Security=True");
+            sqlConn.Open();
+            for (int i = 1; i <= 3; i++)
+            {
+                String strSQL = "UPDATE TableB" + i + " SET tPrice=TableC.tPrice*TableB" + i + ".tNum FROM TableC WHERE TableC.tMeal=TableB" + i + ".tMeal";
+                SqlCommand sqlcommand = new SqlCommand(strSQL, sqlConn);
+                sqlcommand.ExecuteNonQuery();
+                db.SaveChanges();
+            }
+            sqlConn.Close();
+            return View();
+        }
+
+        
+        public IActionResult Accounting()
+        {
+            var accounting = db.TableA.ToList();
+            return View(accounting);
+        }
+
+        public IActionResult DetailsA()
+        {
+            var desk = db.TableB1.Where(n => n.TNum > 0).ToList();
+            return View(desk);
+        }
+        public IActionResult DetailsB()
+        {
+            var desk = db.TableB2.Where(n => n.TNum > 0).ToList();
+            return View(desk);
+        }
+        public IActionResult DetailsC()
+        {
+            var desk = db.TableB3.Where(n => n.TNum > 0).ToList();
+            return View(desk);
+        }
+
+        public IActionResult DeleteA()
+        {
+            SqlConnection sqlConn = new SqlConnection("Data Source=LAPTOP-LD3NQLFM;Initial Catalog=dbORD2;Integrated Security=True");
+            sqlConn.Open();
+            String strSQL = "INSERT INTO TableA (tId,tDesk,tTotal)(SELECT convert(nvarchar(50),GetDate(), 108),1, SUM(tPrice) FROM TableB1) ";
+            SqlCommand sqlcommand = new SqlCommand(strSQL, sqlConn);
+            sqlcommand.ExecuteNonQuery();
+            db.SaveChanges();
+            sqlConn.Close();
+            for (int t = 1; t <= 20; t++)
+            {
+                var Z = db.TableB1.Where(n => n.TNumber == t).FirstOrDefault();
+                Z.TNum = 0;
+                Z.TPrice = 0;
+                db.SaveChanges();
+            }
+            return RedirectToAction("Backstage");
+        }
+        public IActionResult DeleteB()
+        {
+            SqlConnection sqlConn = new SqlConnection("Data Source=LAPTOP-LD3NQLFM;Initial Catalog=dbORD2;Integrated Security=True");
+            sqlConn.Open();
+            String strSQL = "INSERT INTO TableA (tId,tDesk,tTotal)(SELECT convert(nvarchar(50),GetDate(), 108),2, SUM(tPrice) FROM TableB2) ";
+            SqlCommand sqlcommand = new SqlCommand(strSQL, sqlConn);
+            sqlcommand.ExecuteNonQuery();
+            db.SaveChanges();
+            sqlConn.Close();
+            for (int t = 1; t <= 20; t++)
+            {
+                var Z = db.TableB2.Where(n => n.TNumber == t).FirstOrDefault();
+                Z.TNum = 0;
+                Z.TPrice = 0;
+                db.SaveChanges();
+            }
+            return RedirectToAction("Backstage");
+        }
+        public IActionResult DeleteC()
+        {
+            SqlConnection sqlConn = new SqlConnection("Data Source=LAPTOP-LD3NQLFM;Initial Catalog=dbORD2;Integrated Security=True");
+            sqlConn.Open();
+            String strSQL = "INSERT INTO TableA (tId,tDesk,tTotal)(SELECT convert(nvarchar(50),GetDate(), 108),3, SUM(tPrice) FROM TableB3) ";
+            SqlCommand sqlcommand = new SqlCommand(strSQL, sqlConn);
+            sqlcommand.ExecuteNonQuery();
+            db.SaveChanges();
+            sqlConn.Close();
+            for (int t = 1; t <= 20; t++)
+            {
+                var Z = db.TableB3.Where(n => n.TNumber == t).FirstOrDefault();
+                Z.TNum = 0;
+                Z.TPrice = 0;
+                db.SaveChanges();
+            }
+            return RedirectToAction("Backstage");
+        }
+    }
+}
